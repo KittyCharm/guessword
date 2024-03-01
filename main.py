e833cc1ef00963aa63ccb3ff51de7eb65ed7b3e6
KittyCharm <19001732@mail.ecust.edu.cn>
2024-03-01 22:10:47 +0800

    第3，4道筛选存在问题

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 import json
 from search import *
 
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
-
-
 # 按装订区域中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    with open("3-CET4-顺序.json", "r") as json_file:
+    with open("3-CET4-顺序.json", "r", encoding="utf-8") as json_file:
         CET4 = json.load(json_file)
-    with open("4-CET6-顺序.json", "r") as json_file:
+
+    with open("4-CET6-顺序.json", "r", encoding="utf-8") as json_file:
         CET6 = json.load(json_file)
-    with open("5-考研-顺序.json", "r") as json_file:
+    with open("5-考研-顺序.json", "r", encoding="utf-8") as json_file:
         RES = json.load(json_file)
 
+    number = 10
+    data = RES
     result1 = search_by_number_of_letters(number, data)
+    # print(result1)
 
+    letter_position = [[9,'o'],[1,'i'],[2,'n'],[6,'a']]
     result2 = Search_by_letter_that_determines_position(letter_position, result1)
+    # print(result2)
 
+    existing_letters = ''
     result3 = Search_by_existing_letters(existing_letters, result2)
+    # print(result3)
+
+    nonexistent_letters_may = ''
+
+    nonexistent_letters=''
+    for i in nonexistent_letters_may:
+        if i in existing_letters:
+            continue
+        nonexistent_letters=nonexistent_letters+i
 
     result4 = Exclude_by_nonexistent_letters(nonexistent_letters, result3)
+    print(result4)
--- search.py
+++ search.py
@@ -1,10 +1,50 @@
 def search_by_number_of_letters(number, data):
+    result = []
+    for i in data:
+        if len(i['word']) == number:
+            result.append(i['word'])
+    return result
 
 
 def Search_by_letter_that_determines_position(letter_position, data):
+    result = []
+    for i in data:
+        t = 0
+        for j in letter_position:
+            if i[j[0] - 1] != j[1]:
+                t = 1
+                break
+        if t == 1:
+            continue
+        result.append(i)
+    return result
 
 
 def Search_by_existing_letters(existing_letters, data):
+    result = []
+    for i in data:
+        t = 0
+        for j in existing_letters:
+            if j in i:
+                continue
+            else:
+                t = 1
+                break
+        if t == 1:
+            continue
+        result.append(i)
+    return result
 
 
 def Exclude_by_nonexistent_letters(nonexistent_letters, data):
+    result = []
+    t = 0
+    for i in data:
+        for j in nonexistent_letters:
+            if j in i:
+                t = 1
+                break
+        if t == 1:
+            continue
+        result.append(i)
+    return result
